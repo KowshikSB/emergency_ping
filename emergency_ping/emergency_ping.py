f38055b0dc6b67f5e55e0ef5be1656a9d1fcc024
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-04-14 18:38:08 +0530

    Update emergency_ping.py

--- emergency_ping/emergency_ping.py
+++ emergency_ping/emergency_ping.py
@@ -13,20 +13,20 @@ class em(commands.Cog):
 
     
     @commands.command()
-    async def emergency(self,message,*,reason=None):
+    async def emergency(self,*,reason=None):
         if reason is not None:
-            em=discord.Embed(title ="Emergency Situation", colour=0x2f3136,timestamp=self.message.created_at)
+            em=discord.Embed(title ="Emergency Situation", colour=0x2f3136,timestamp=reason.created_at)
             
             icon=self.guild.icon_url
             em.set_thumbnail(url=icon)
             em.add_field(name="Reason:",value=f"```{reason}```",inline=False)
-            em.add_field(name="Triggered by:",value=f"<@{message.author.id}>",inline=True)
+            em.add_field(name="Triggered by:",value=f"<@{reason.author.id}>",inline=True)
             guild=self.get_guild(799526257506254868)
-            await message.add_reaction("<:emergency_ping:831873364087537664>")
+            await reason.message.add_reaction("<:emergency_ping:831873364087537664>")
             x="<@&825260273010081794>"
-            await message.send(x,embed=em)
+            await reason.channel.send(x,embed=em)
         else:
-            await message.send("*Please mention the reason*")
+            await reason.channnel.send("*Please mention the reason*")
         
 def setup(bot):
     bot.add_cog(em(bot))
